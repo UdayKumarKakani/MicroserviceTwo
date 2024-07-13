ee534ca0d4cdfe672c48a7d5b9f594c1a7fd9054
Uday Kumar <38879020+UdayKumarKakani@users.noreply.github.com>
2024-07-13 09:57:47 +0530

    Update WeatherForecastController.cs

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -25,7 +25,7 @@ public class WeatherForecastController : ControllerBase
         {
             Date = DateTime.Now.AddDays(index),
             TemperatureC = Random.Shared.Next(-20, 55),
-            Summary = "Data From Microservice Two"
+            Summary = "Data From Microservice Two For Jenkins"
         })
         .ToArray();
     }
