6bc3d32670d65f4a0294948ffed97a5b7a7f1a7f
Uday Kumar <38879020+UdayKumarKakani@users.noreply.github.com>
2024-08-21 21:47:11 +0530

    Implemented new chnages

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -19,14 +19,20 @@ public class WeatherForecastController : ControllerBase
     }
 
     [HttpGet(Name = "GetWeatherForecast")]
-    public IEnumerable<WeatherForecast> Get()
+   public ActionResult<IList<WeatherForecast>> Get()
     {
-        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+        IList<WeatherForecast> weatherForecast= new List<WeatherForecast>();
+        for (int i = 0; i < 10; i++)
         {
-            Date = DateTime.Now.AddDays(index),
-            TemperatureC = Random.Shared.Next(-20, 55),
-            Summary = "Data From Microservice Two"
-        })
-        .ToArray();
+            WeatherForecast weather = new WeatherForecast();
+            weather.ProductName= "Product-" + i;
+            weather.ProductDescription= "Order Description-" + i;
+            weather.Date= DateTime.Now;
+            weather.Cost= i;
+            weather.Count= i+ 1;
+            weatherForecast.Add(weather);
+        }
+        return Ok(weatherForecast);
     }
+
 }
--- WeatherForecast.cs
+++ WeatherForecast.cs
@@ -4,9 +4,10 @@ public class WeatherForecast
 {
     public DateTime Date { get; set; }
 
-    public int TemperatureC { get; set; }
+    public string? ProductName { get; set; }
 
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+    public string? ProductDescription { get; set; }
 
-    public string? Summary { get; set; }
+    public int  Count { get; set; }
+     public int  Cost { get; set; }
 }
